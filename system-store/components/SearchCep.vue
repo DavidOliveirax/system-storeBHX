9eef6f3a6745ad508640439245541dabe19c9c65
DavidOliveirax <davidoliveirax92@gmail.com>
2024-04-14 11:11:21 -0300

    Fixes 13/04

---
+++ system-store/components/SearchCep.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-72 pb-2.5 pr-1">
+        <label for="UserCep"
+            class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+            <span class="text-xs font-medium text-white"> CEP: </span>
+            <input type="number" id="UserCep" placeholder=""
+                class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
+        </label>
+    </div>
+</template>
