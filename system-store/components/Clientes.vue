7dc271c2a44943f591dd4e725c286639741ce2ed
David Oliveira <149318831+DavidOliveirax@users.noreply.github.com>
2024-05-14 20:33:45 -0300

    Fixes 14-05

--- system-store/components/Clientes.vue
+++ system-store/components/Clientes.vue
@@ -3,35 +3,36 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
     <div class="flex justify-center mt-9 ml-9">
         <form action="form.py" method="GET" autocomplete="on" class="form" id="formulario">
             <div class="flex">
+
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserName"
+                    <label for="userName"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> Nome: </span>
-                        <input type="name" id="UserName" placeholder=""
+                        <input v-model="name" type="name" id="userName" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserSocial"
+                    <label for="userSocial"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> Razão Social: </span>
-                        <input type="name" id="UserSocial" placeholder=""
+                        <input v-model="social" type="name" id="UserSocial" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserDocument"
+                    <label for="userDocument"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> CPF/CNPJ: </span>
-                        <input type="number" id="UserDocument" placeholder=""
+                        <input v-model="document" type="number" id="UserDocument" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserEmail"
+                    <label for="userEmail"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Email: </span>
-                        <input type="email" id="UserEmail" placeholder=""
+                        <input v-model="email" type="email" id="UserEmail" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
@@ -41,37 +42,60 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
             </div>
             <div class="flex">
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserLocal"
+                    <label for="userLocal"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Endereço: </span>
-                        <input type="text" id="UserLocal" placeholder=""
+                        <input v-model="local" type="text" id="UserLocal" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserBairro"
+                    <label for="userBairro"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Bairro: </span>
-                        <input type="text" id="UserBairro" placeholder=""
+                        <input v-model="district" type="text" id="UserBairro" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
                 <div class="w-72 pb-2.5 pr-1">
-                    <label for="UserCidade"
+                    <label for="userCidade"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Cidade: </span>
-                        <input type="text" id="UserCidade" placeholder=""
+                        <input v-model="city" type="text" id="UserCidade" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
                 <div>
                     <ChangeState />
                 </div>
+                <a class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+                    href="#">
+                    Enviar
+                </a>
             </div>
+            Nome {{ name }} <br>
+            Razão Social {{ social }} <br>
+            Documento {{ document }} <br>
+            Email {{ email }} <br>
+            Endereço {{ local }} <br>
+            Bairro {{ district }} <br>
+            Cidade {{ city }} <br>
         </form>
     </div>
 </template>
 
 <script>
-    
+export default {
+    data() {
+        return {
+            name: "",
+            social: "",
+            document: "",
+            email: "",
+            local: "",
+            district: "",
+            city: "",
+        }  
+    }
+}
 </script>
