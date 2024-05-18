09ab07558f0a8f3c440f9a831c42ccff61c402ba
David Oliveira <149318831+DavidOliveirax@users.noreply.github.com>
2024-05-17 23:37:19 -0300

    Fixes 17-05

--- system-store/components/ClientesP.vue
+++ system-store/components/ClientesP.vue
@@ -1,14 +1,14 @@
 import BuscadorCep from './Dropdown/BuscadorCep.vue';
 <template>
     <div class="flex justify-center mt-9 ml-9">
-        <form action="form.py" method="GET" autocomplete="on" class="form" id="formulario">
+        <form>
             <div class="flex">
 
                 <div class="w-72 pb-2.5 pr-1">
                     <label for="userName"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> Nome: </span>
-                        <input v-model="name" type="name" id="userName" placeholder=""
+                        <input v-model="userData.name" type="name" id="userName" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
@@ -16,7 +16,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userSocial"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> Razão Social: </span>
-                        <input v-model="social" type="text" id="UserSocial" placeholder=""
+                        <input v-model="userData.social" type="text" id="UserSocial" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
@@ -24,7 +24,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userDocument"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-60">
                         <span class="text-xs font-medium text-white"> CPF/CNPJ: </span>
-                        <input v-model="document" type="number" id="UserDocument" placeholder=""
+                        <input v-model="userData.document" type="number" id="UserDocument" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm">
                     </label>
                 </div>
@@ -32,7 +32,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userEmail"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Email: </span>
-                        <input v-model="email" type="email" id="UserEmail" placeholder=""
+                        <input v-model="userData.email" type="email" id="UserEmail" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
@@ -45,7 +45,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userLocal"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Endereço: </span>
-                        <input v-model="local" type="text" id="UserLocal" placeholder=""
+                        <input v-model="userData.local" type="text" id="UserLocal" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
@@ -53,7 +53,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userBairro"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Bairro: </span>
-                        <input v-model="district" type="text" id="UserBairro" placeholder=""
+                        <input v-model="userData.district" type="text" id="UserBairro" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
@@ -61,7 +61,7 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
                     <label for="userCidade"
                         class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                         <span class="text-xs font-medium text-white"> Cidade: </span>
-                        <input v-model="city" type="text" id="UserCidade" placeholder=""
+                        <input v-model="userData.city" type="text" id="UserCidade" placeholder=""
                             class="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm" />
                     </label>
                 </div>
@@ -73,35 +73,39 @@ import BuscadorCep from './Dropdown/BuscadorCep.vue';
             <div class="flex justify-end pt-2.5">
                 <button
                     class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                    type="submit" @click="submit">
+                    type="submit" @click.prevent="submit">
                     Enviar
                 </button>
             </div>
-            Nome {{ name }} <br>
-            Razão Social {{ social }} <br>
-            Documento {{ document }} <br>
-            Email {{ email }} <br>
-            Endereço {{ local }} <br>
-            Bairro {{ district }} <br>
-            Cidade {{ city }} <br>
+            Nome {{ userData.name }} <br>
+            Razão Social {{ userData.social }} <br>
+            Documento {{ userData.document }} <br>
+            Email {{ userData.email }} <br>
+            Endereço {{ userData.local }} <br>
+            Bairro {{ userData.district }} <br>
+            Cidade {{ userData.city }} <br>
         </form>
     </div>
 </template>
 
-<script setup>
-const name = defineModel("name", {default: ""})
-const social = defineModel("social", {default: ""})
-const document = defineModel("document", {default: ""})
-const email = defineModel("email", {default: ""})
-const local = defineModel("local", {default: ""})
-const district = defineModel("district", {default: ""})
-const city = defineModel("city", {default: ""})
+<script setup lang="ts">
+const userData = ref({
+    name: "",
+    social: "",
+    document: "",
+    email: "",
+    local: "",
+    district: "",
+    city: "",
+})
+
 
 function submit() {
     console.log('Cadastro Enviado!')
+    console.log(userData.value)
 }
 
-function emailValid(email) {
+function emailValid(email: string) {
     const emailRegex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,}$/)
     if(emailRegex.test(email)) {
         return true
